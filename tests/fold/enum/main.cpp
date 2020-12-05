349bdc956b21a56da0899bd83a2a6ffd4952dafe
jean-mathieu vermosen <vermosen@yahoo.com>
2020-12-05 13:06:12 +0100

    added test

--- tests/fold/enum/main.cpp
+++ tests/fold/enum/main.cpp
@@ -45,6 +45,40 @@ TEST(unittest, meta_fold_enum_class) {
   ASSERT_EQ(std::get<2>(checks), false);
 }
 
+template <typename Arg, type T>
+struct element_ext;
+
+template <typename Arg>
+struct element_ext<Arg, type::foo> {
+  void apply(std::tuple<bool, bool, bool>& check) { std::get<0>(check) = true; }
+};
+
+template <typename Arg>
+struct element_ext<Arg, type::bar> {
+  void apply(std::tuple<bool, bool, bool>& check) { std::get<1>(check) = true; }
+};
+
+template <typename Arg>
+struct element_ext<Arg, type::baz> {
+  void apply(std::tuple<bool, bool, bool>& check) { std::get<2>(check) = true; }
+};
+
+struct arg {};
+
+template <type Type>
+using alias_t = element_ext<arg, Type>;
+
+TEST(unittest, meta_fold_enum_alias) {
+
+  using mapper_t = mapper<alias_t, type::foo, type::bar>;
+  mapper_t map;
+  std::tuple<bool, bool, bool> checks = { false, false, false };
+  map.apply_all(checks);
+  ASSERT_EQ(std::get<0>(checks), true);
+  ASSERT_EQ(std::get<1>(checks), true);
+  ASSERT_EQ(std::get<2>(checks), false);
+}
+
 int main(int argc, char* argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
