bcbe78f13c687c66dc6a0f06bd6216cec3603748
jean-mathieu vermosen <vermosen@yahoo.com>
2021-02-10 10:39:31 +0100

    bump to 0.1.12

--- src/meta/preprocessor/pair.h
+++ src/meta/preprocessor/pair.h
@@ -16,7 +16,7 @@
 
 
 #define META_FOR_EACH_DECLARE_PAIR(X, SEQ)                      \
-	BOOST_PP_SEQ_FOR_EACH_I(OTOS_DECLARE_PAIR                   \
-        , X, OTOS_SEQ_DOUBLE_PARENS(SEQ))
+	BOOST_PP_SEQ_FOR_EACH_I(META_DECLARE_PAIR                     \
+        , X, META_SEQ_DOUBLE_PARENS(SEQ))
 
 #endif // META_PREPROCESSOR_PAIR_H
